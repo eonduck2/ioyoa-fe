0dfe008a2329e1d99258adf9c2b16eb21370657d
이종수 <whdtnxd@gmail.com>
2024-10-07 11:12:49 +0900

    :package: circleMenu 정적 타입 임포트 -> 별칭 사용

--- static/components/menus/circleMenu.static.ts
+++ static/components/menus/circleMenu.static.ts
@@ -1,4 +1,4 @@
-import type { TMenuItems } from "../../../src/types/static/components/menus/menuItems.type";
+import type { TMenuItems } from "~/types/static/components/menus/menuItems.type";
 
 export const menuItems: TMenuItems[] = [
   { icon: "🏠", label: "홈", href: "/" },
