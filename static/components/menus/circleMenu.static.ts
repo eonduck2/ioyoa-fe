49f7efa69fc066dd44f1868726ec07fed95108b9
eonduck2 <whdtnxd@gmail.com>
2024-10-16 14:47:09 +0900

    :package: 상영관 -> cinema 정적 자원 변경

--- static/components/menus/circleMenu.static.ts
+++ static/components/menus/circleMenu.static.ts
@@ -3,7 +3,7 @@ import type { TMenuItems } from "~/types/static/components/menus/menuItems.type"
 export const menuItems: TMenuItems[] = [
   { icon: "🔍", label: "검색", href: "/search" },
   { icon: "🔥", label: "인기", href: "/mostPopular" },
-  { icon: "📽️", label: "상영관", href: "/" },
+  { icon: "📽️", label: "Cinema", href: "/" },
   { icon: "📌", label: "Picker", href: "/" },
   { icon: "🤖", label: "Jammer AI", href: "/" },
 ];
